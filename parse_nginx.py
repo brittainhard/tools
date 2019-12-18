d909856deafca8d58ecefe9c65c3d042560d88db
Brittain Hard <brittainhard@gmail.com>
2019-12-18 12:18:57 -0600

    Don't really need these scripts.

--- parse_nginx.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a6f44945a8cb988a3d90a2012f6b063470fa9f76c2006ce2576dc79822276337
-size 308
--- scratch.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2716e9aaa2ba90397a799f2f5a542805e98b07541f8ccaa2c6d1e8b24af32156
-size 277
